12dbd9fedab2f7f3c2e3d6fe62348334450e13d4
Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
2012-10-17 23:19:29 +0200

    [fuse] correct fuse settings for atmega328p

--- avr/main.c
+++ avr/main.c
@@ -1,10 +1,17 @@
 #include <avr/interrupt.h>
+#include <avr/power.h>
+#include <util/delay.h>
+
 #include "rfm12.h"
 
 int main(void)
 {
 	cli();
+	_delay_ms(10);
+
 	rfm12_init();
+	// the clk/8 fuse bit is set
+	clock_prescale_set(clock_div_1);
 	sei();
 
 	while(1) {
