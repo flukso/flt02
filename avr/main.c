5630ca1801ca6a4e1ab7431acf4ba6a0d53a7338
Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
2012-10-19 21:39:40 +0200

    [pkt] define a first packet type

--- avr/main.c
+++ avr/main.c
@@ -3,6 +3,39 @@
 #include <util/delay.h>
 
 #include "rfm12.h"
+#include "pkt.h"
+
+static pkt_1_t pkt_counter = {
+	.head = {
+		.grp = PKT_GRP, //hardcoded for the time being
+		.hdr = PKT_NID, //CTL=0, DST=0, ACK=0 -> b'cast noack
+		.len = PKT_1_LEN,
+		.typ = PKT_1_TYP
+	},
+
+	.sid = 1,
+	.count = 2,
+	.msec = 3
+};
+
+static int send(uint8_t *pkt, uint8_t size)
+{
+	uint8_t i, ret;
+
+	//we cannot busy loop here since this might cause
+	//a deadlock with rfm12_tick in the main loop
+	//the compiler seems to be quite fond of all these parentheses
+	if ((ret = rfm12_tx_occupy()))
+		return ret;
+
+	for (i = 0; i < size; i++) {
+		rfm12_tx_buffer_add(pkt[i]);
+	}
+
+	rfm12_tx_start();
+
+	return 0;
+}
 
 int main(void)
 {
@@ -14,7 +47,9 @@ int main(void)
 	clock_prescale_set(clock_div_1);
 	sei();
 
-	while(1) {
+	send((uint8_t *) &pkt_counter, sizeof(pkt_counter));
+
+	while (1) {
 		rfm12_tick();
 	}
 }
---
+++ avr/pkt.h
@@ -0,0 +1,20 @@
+#define PKT_GRP		0xd4
+#define PKT_NID		0x01
+
+typedef struct {
+	uint8_t grp;
+	uint8_t hdr;
+	uint8_t len;
+	uint8_t typ;
+} pkt_head_t;
+
+
+#define PKT_1_LEN	10
+#define PKT_1_TYP	1
+
+typedef struct {
+	pkt_head_t head;
+	uint8_t sid; //local sensor id
+	uint32_t count;
+	uint32_t msec;
+} pkt_1_t;
